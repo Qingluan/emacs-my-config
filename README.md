0d1eb8173338b30ef7a1542bb9363fde6e7b351b
Your Name <you@example.com>
2018-05-14 16:44:30 +0800

    finish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # emacs-my-config
 a emacs25 config 
+
+> this is config from my friends 's config , i think it is a stable version to use.
+> you can use for python , scala , w3m , and so on .
+
---
+++ init.el
@@ -0,0 +1,11 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load-file custom-file)
+
+(org-babel-load-file "~/.emacs.d/dotemacs.org")
